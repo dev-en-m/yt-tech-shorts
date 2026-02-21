18e0b5dd97b3902d9f97bf099e80bd004c535856
dev-en-m <devendramulewa171@gmail.com>
2026-02-21 17:19:21 +0530

    Added sqlite support for videos also

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import csv from "csv-parser";
 import fs from "fs";
 import Database from "better-sqlite3";
-
+import { exit } from "process";
 const db = new Database("app.db");
 
 // 1. Ensure table exists (safe to run every time)
@@ -13,6 +13,41 @@ db.prepare(
   )
 `,
 ).run();
+
+db.prepare(
+  `
+   CREATE TABLE IF NOT EXISTS videos (
+    video_id TEXT PRIMARY KEY,
+    channel_id TEXT NOT NULL,
+    title TEXT NOT NULL,
+    published_at TEXT NOT NULL, 
+    is_short INTEGER NOT NULL  
+  )
+`,
+).run();
+
+async function getNewVideos() {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    fs.createReadStream("./csv/videos-list.csv")
+      .pipe(csv())
+      .on("data", (result) => results.push(result))
+      .on("end", () => resolve(results))
+      .on("error", (error) => reject(error));
+  });
+}
+
+export async function getChannelActivity() {
+  try {
+    const channel_activities = db
+      .prepare(`SELECT * FROM channel_activity`)
+      .all();
+    return channel_activities;
+  } catch (error) {
+    console.error("getChannelActity Error", error);
+    exit(1);
+  }
+}
 
 export async function updateChannelActivity(videos) {
   const latestByChannel = new Map();
@@ -42,4 +77,35 @@ export async function updateChannelActivity(videos) {
   transaction();
 
   console.log("db operation complete");
+}
+
+export function closeDb() {
+  db.close();
+  console.log("Database connection closed");
+}
+
+export async function saveVideos(videos) {
+  try {
+    const insertStmt = db.prepare(`
+    INSERT OR IGNORE INTO videos (video_id, channel_id, title, published_at, is_short)
+    VALUES (?, ?, ?, ?, ?)
+  `);
+
+    const transaction = db.transaction(() => {
+      for (const element of videos) {
+        insertStmt.run(
+          element.video_id,
+          element.channel_id,
+          element.title,
+          element.published_at,
+          element.is_short === "true" || element.is_short === true ? 1 : 0,
+        );
+      }
+    });
+
+    transaction();
+    console.log(`Inserted ${videos.length} videos`);
+  } catch (error) {
+    console.error(error);
+  }
 }
